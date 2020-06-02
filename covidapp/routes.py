f1adee015475b34513df94f55d39fb66a06dfc6b
Ziaee AmirAbbas <ziaeeamir@fh-kufstein.ac.at>
2020-06-02 19:13:39 +0200

    code cleanded

--- covidapp/routes.py
+++ covidapp/routes.py
@@ -12,24 +12,25 @@ import json, plotly
 @app.route('/', methods=['POST', 'GET'])
 @app.route('/index', methods=['POST', 'GET'])
 def index():
-
     default_county  = ['United_States_of_America']
 
+    # get the dataset
     dataset = get_dataset()
+    # prepare the map dataset
     map_data = set_map_data(dataset)
     five_top_countries, country_codes = get_the_top(map_data)
 
-
+    # to get the five top countries
     five_top_countries = [country for country, code in five_top_countries]
     dates=[dataset.index.min(), dataset.index.max()]
-    # the five top countries filter
     five_top_df = filter_data(dataset, country_name = five_top_countries, date = dates  )
+
+    # to get the initial charts (map and the five top countries)
     figures = return_figures(map_data, five_top_df)
 
 
 
-        #figures.append(figure.items())
-        # Parse the POST request countries list
+    # Parse the POST request country list
     if (request.method == 'POST') and request.form :
          # the user selected country
         filter_df = filter_data(dataset, country_name = list(request.form.values()), date = dates  )
@@ -37,9 +38,11 @@ def index():
         default_county = list(request.form.values())
 
     else:
+        # the default user selected country chart
         filter_df = filter_data(dataset, country_name = default_county, date = dates  )
         user_selected_figure= return_figures(map_data, filter_df)
 
+    # to add the default user selected charts to into figures
     for figure in user_selected_figure:
         figures.append(figure)
 
